e14a2f44172c500f4aa9ba8ce54ceae12e91de54
ninathe <nina.th.93@gmail.com>
2017-06-16 08:45:46 +0200

    jquery, bootstrap and mapbox scripts and stylesheets included

--- Public/javascripts/map.js
+++
@@ -1,2 +0,0 @@
-// JavaScript source code
-//# sourceMappingURL=map.js.map
---
+++ Public/scripts/map.ts
@@ -0,0 +1,30 @@
+// JavaScript source code
+// var mapboxgl:any;
+var mapboxgl;
+window.onload = function () { 
+  mapboxgl.accessToken = 'pk.eyJ1IjoibmluYXRoZSIsImEiOiJjaWp4OGZhMnUwMDI2dWxtMHhrZWVhYm1vIn0.Uff1efmgDOAOJ-nFVHAUgQ';
+  var map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v9'
+  });
+
+  map.addControl(new mapboxgl.Navigation({position: 'bottom-right'}));
+  map.addControl(new mapboxgl.Geolocate({position: 'bottom-right'}));
+  addTitlesControllers();
+}
+
+
+
+
+
+function addTitlesControllers(){
+  var locationBtn = document.getElementsByClassName("mapboxgl-ctrl-geolocate")[0];
+  var zoomIn = document.getElementsByClassName("mapboxgl-ctrl-zoom-in")[0];
+  var zoomOut = document.getElementsByClassName("mapboxgl-ctrl-zoom-out")[0];
+  var rotation = document.getElementsByClassName("mapboxgl-ctrl-compass")[0];
+
+//   $(locationBtn).attr('title','Finn din posisjon');
+//   $(zoomIn).attr('title','Zoom inn');
+//   $(zoomOut).attr('title','Zoom ut');
+//   $(rotation).attr('title','Roter kart');
+}
